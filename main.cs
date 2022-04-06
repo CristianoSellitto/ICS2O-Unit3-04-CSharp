67236df1fdee5fb4223b4cdf190a40f7e12425a6
CristianoSellit <99354780+CristianoSellitto@users.noreply.github.com>
2022-04-06 17:29:29 +0000

    Added program

--- main.cs
+++ main.cs
@@ -8,7 +8,33 @@ class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello World");
+        string selection;
+        double answer;
+
+        Console.WriteLine("Do you want to convert celsius or fahrenheit?");
+        selection = Console.ReadLine();
+        if (selection == "celsius")
+        {
+            double celsius;
+
+            Console.WriteLine("Enter temperature in celsius:");
+            celsius = Convert.ToSingle(Console.ReadLine());
+            answer = (celsius * 9/5) + 32;
+            Console.WriteLine("This temperature is equal to " + answer + "°F");
+        }
+        else if (selection == "fahrenheit")
+        {
+            double fahrenheit;
+
+            Console.WriteLine("Enter temperature in fahrenheit:");
+            fahrenheit = Convert.ToSingle(Console.ReadLine());
+            answer = (fahrenheit - 32) * (5/9);
+            Console.WriteLine("This temperature is equal to " + answer + "°C");
+        }
+        else
+        {
+            Console.WriteLine("Please specify if you want to convert celsius or fahrenheit. You may have made a typo.");
+        }
         Console.WriteLine("\nFinished.");
     }
 }
